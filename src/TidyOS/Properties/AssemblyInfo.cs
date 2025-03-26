b3d19df9d3279040d7bca00e6f7606100e3ce57e
Belim <belim@builtbybel.com>
2025-03-26 17:25:34 +0100

    Update AssemblyInfo.cs

--- src/TidyOS/Properties/AssemblyInfo.cs
+++ src/TidyOS/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Buildnummer
 //      Revision
 //
-[assembly: AssemblyVersion("0.20.40")]
-[assembly: AssemblyFileVersion("0.20.40")]
+[assembly: AssemblyVersion("0.30.63")]
+[assembly: AssemblyFileVersion("0.30.63")]
